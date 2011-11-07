87c7912af267826e0d6adb9efd3e4525d499ae10
C. Corey Capel <corey@capelio.com>
2011-11-07 00:48:24 -0700

    Add base RefineryCMS files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WcnhWeb::Application.config.secret_token = 'd03b096cefe35ac134f5c04d49288e3874a94d4eae94e083acb875d17c2bc66535df011a5dffeb62bd79c09af5b93dea284207d3225eaaf470313b5a85eeba92'
