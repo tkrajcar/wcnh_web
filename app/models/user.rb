beef96d3ba9a7a4895f31cb4c7364d6ace482b1f
Tim Krajcar <allegro@conmolto.org>
2012-02-01 21:27:41 -0800

    add current_dbref field to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   field :name
+  field :current_dbref
 
-  validates_presence_of :name
+  validates_presence_of :name, :current_dbref
 
-  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates_uniqueness_of :name, :email, :current_dbref, :case_sensitive => false
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
