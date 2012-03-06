fe224659d0e113aa9d13bb29de379a040f2bbf8b
Tim Krajcar <allegro@conmolto.org>
2012-03-06 10:41:46 -0800

    implement page title code with helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:page_title) { page_title }
+  end
+  def h1(h1)
+    content_for(:page_h1) { h1 }
+  end
+  def title_and_h1(text)
+    content_for(:page_title) { text }
+    content_for(:page_h1) { text }
+  end
 end
