2eed3379ed796d0b5b7e9b68753bead85837ea80
Brandon Rice <brice84@gmail.com>
2012-06-24 19:20:04 -0400

    Make DateTime.to_ictime available to Rails.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load lib/extensions.rb for some custom class stuff
+require 'extensions'
+
 # Initialize the rails application
 WcnhWeb::Application.initialize!
